c2e724b59b5654346175d80863533a194d22ee58
Marion Grandjean <grandjean.marion@gmail.com>
2015-06-19 14:30:19 +0200

    Fixing for heroku

--- Gemfile
+++ Gemfile
@@ -57,4 +57,3 @@ group :development do
   gem "better_errors"
   gem "binding_of_caller"
 end
-gem 'rspec-rails', group: [ :test ]
--- db/migrate/20150618150929_remove_ingredient_id_from_doses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIngredientIdFromDoses < ActiveRecord::Migration
-  def change
-    remove_column :doses, :ingredient_id, :integer
-  end
-end
